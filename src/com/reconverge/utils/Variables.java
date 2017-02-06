b6e2c3742e3ab882632a0a3aa038c3d374afc072
madHEYsia\submi <submiitr07@gmail.com>
2017-02-06 17:41:54 +0530

    Changing build environment from sublime to Intellij IDE

--- src/com/reconverge/utils/Variables.java
+++ src/com/reconverge/utils/Variables.java
@@ -1,4 +1,4 @@
-// package Variables;
+ package com.reconverge.utils;
 
 public class Variables {
 	public static String MAIN_WINDOW_ICON = "C:/Users/submi/Desktop/GUI/GUI/src/main/resources/images/icon.png";	
