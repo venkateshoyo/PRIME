f068ae79ed69912d68ce6b36440d1f6903624f36
madHEYsia\submi <submiitr07@gmail.com>
2017-02-10 22:04:39 +0530

    Indentation correcction

--- src/com/reconverge/main/functions/close/closewindow.java
+++ src/com/reconverge/main/functions/close/closewindow.java
@@ -1,9 +1,11 @@
 package com.reconverge.main.functions.close;
 
 import com.reconverge.main.main;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
@@ -12,44 +14,49 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class closewindow{
-        public void confirmclose(){
 
-                main ob = new main();
+    public void confirmclose(){
+        main ob = new main();
+        Stage confirm = new Stage();
+        confirm.initModality(Modality.APPLICATION_MODAL);
 
-                Stage confirm = new Stage();
-                confirm.initModality(Modality.APPLICATION_MODAL);
+        Label text= new Label("Are you sure you want to exit ?");
+        text.setId("labeltext");
+        confirm.setTitle("Confirm");
 
-                Label text= new Label("Are you sure you want to exit ?");
-                text.setId("labeltext");
-                confirm.setTitle("Confirm");
 
-                Button yes = new Button("Yes");
-                yes.setId("yes");
-                yes.setOnAction(e -> {
-                        confirm.close();
-                        ob.getstage().close();
-                });
+        Button yes = new Button("Yes");
+        yes.setId("yes");
+        yes.setOnAction(e -> {
+            confirm.close();
+            ob.getstage().close();
+        });
 
-                Button no = new Button("No");
-                no.setId("no");
-                no.setOnAction(e -> confirm.close());
+        Button no = new Button("No");
+        no.setId("no");
+        no.setOnAction(e -> confirm.close());
 
-                HBox layout = new HBox();
-                layout.setAlignment(Pos.CENTER);
-                layout.setSpacing(35);
-                layout.getChildren().addAll(yes,no);
+        HBox layout = new HBox();
+        layout.setAlignment(Pos.CENTER);
+        layout.setSpacing(30);
+        layout.setPadding(new Insets(15,35,10,35));
+        layout.getChildren().addAll(yes,no);
 
-                VBox layout11 = new VBox();
-                layout11.setAlignment(Pos.CENTER);
-                layout11.setSpacing(20);
-                layout11.getChildren().addAll(text,layout);
+        CheckBox dontask = new CheckBox("Don't ask me again");
+        dontask.setId("dontagain");
+        dontask.setSelected(true);
 
-                Scene scene= new Scene(layout11,350,150);
+        VBox layout11 = new VBox();
+        layout11.setAlignment(Pos.CENTER);
+        layout11.setSpacing(5);
+        layout11.getChildren().addAll(text,layout, dontask);
 
-                confirm.setScene(scene);
-                confirm.show();
-                confirm.setResizable(false);
-                confirm.getIcons().add(new Image(getClass().getResourceAsStream("no.png")));
-                scene.getStylesheets().add(closewindow.class.getResource("closecss.css").toExternalForm());
-        }
+        Scene scene= new Scene(layout11,250,150);
+
+        confirm.setScene(scene);
+        confirm.show();
+        confirm.setResizable(false);
+        confirm.getIcons().add(new Image(getClass().getResourceAsStream("no.png")));
+        scene.getStylesheets().add(closewindow.class.getResource("closecss.css").toExternalForm());
+    }
 }
