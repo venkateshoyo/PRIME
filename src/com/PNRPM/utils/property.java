06e12676e69a651618d74371616a9e501f437500
madHEYsia\submi <submiitr07@gmail.com>
2017-04-05 10:43:32 +0530

    WIP

--- src/com/PNRPM/utils/property.java
+++ src/com/PNRPM/utils/property.java
@@ -1,14 +1,8 @@
 package com.PNRPM.utils;
 
 public class property {
-    public static final String ROOT_NODE = "com.PNRPM";
-    public static final String CONTACT_PHONE_NUMBER = "+91-9756526651";
-    public static final String CONTACT_EMAIL = "submiitr07@gmail.com";
-
     public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     public static final String DB_URL = "jdbc:mysql://localhost/pnrpm";
-    public static final String DB_DEFAULT_DB = "jdbc:mysql://localhostr/pnrpm";
     public static final String DB_USERNAME = "root";
     public static final String DB_PASSWORD = "root";
-
 }
