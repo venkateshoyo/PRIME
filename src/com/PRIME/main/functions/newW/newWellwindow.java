e5f1d7c465ce9c3e03080995a07c46dba8690f16
madHEYsia\submi <submiitr07@gmail.com>
2017-04-08 00:55:36 +0530

    Allignment update

--- src/com/PRIME/main/functions/newW/newWellwindow.java
+++ src/com/PRIME/main/functions/newW/newWellwindow.java
@@ -5,14 +5,10 @@ import com.PRIME.database.utils.DBUtils;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.sql.ResultSet;
@@ -26,11 +22,13 @@ public class newWellwindow {
 
         BorderPane wellview = new BorderPane();
 
+        VBox vbHeader = new VBox(10);
+
         BorderPane grid = new BorderPane();
         grid.setPadding(new Insets(15));
 
         Label file = new Label("File");
-        file.setPadding(new Insets(0,25,0,0));
+        file.setPadding(new Insets(5,25,0,0));
         grid.setLeft(file);
 
         final ComboBox<String> combo = new ComboBox<String>();
@@ -45,13 +43,37 @@ public class newWellwindow {
         combo.setEditable(true);
         grid.setCenter(combo);
 
-        HBox hb = new HBox(25);
-        Label lb = new Label("");
+        HBox hbBrowse = new HBox(25);
+        Label space = new Label("");
         Button Browse = new Button("Browse");
-        hb.getChildren().addAll(lb,Browse);
-        grid.setRight(hb);
+        hbBrowse.getChildren().addAll(space,Browse);
+        grid.setRight(hbBrowse);
 
-        wellview.setTop(grid);
+        HBox hbWellBy = new HBox(30);
+        hbWellBy.setPadding(new Insets(0,15,0,0));
+
+        Label wellBy = new Label("Import Well By: ");
+
+        HBox radioButton = new HBox(20);
+        radioButton.setPadding(new Insets(0));
+        final ToggleGroup group = new ToggleGroup();
+        RadioButton rb1 = new RadioButton("Well Name");
+        rb1.setToggleGroup(group);
+        rb1.setSelected(true);
+        RadioButton rb2 = new RadioButton("API ID");
+        rb2.setToggleGroup(group);
+        RadioButton rb3 = new RadioButton("Well ID");
+        rb3.setToggleGroup(group);
+        radioButton.getChildren().addAll(rb1,rb2,rb3);
+
+        Button wellImport = new Button("Import");
+
+        hbWellBy.getChildren().addAll(wellBy,radioButton,wellImport);
+        hbWellBy.setAlignment(Pos.BASELINE_RIGHT);
+
+        vbHeader.getChildren().addAll(grid,hbWellBy);
+
+        wellview.setTop(vbHeader);
 
         Scene scene = new Scene(wellview);
         wellload.setScene(scene);
