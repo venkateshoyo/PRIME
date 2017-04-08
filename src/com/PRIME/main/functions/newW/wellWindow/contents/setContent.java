52f653599d3a190051b7e850756632e2d2a2eec2
madHEYsia\submi <submiitr07@gmail.com>
2017-04-08 23:14:55 +0530

    responsive column header added to set content

--- src/com/PRIME/main/functions/newW/wellWindow/contents/setContent.java
+++ src/com/PRIME/main/functions/newW/wellWindow/contents/setContent.java
@@ -1,16 +1,37 @@
 package com.PRIME.main.functions.newW.wellWindow.contents;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class setContent {
 
     public static BorderPane setContent(){
-        BorderPane set = new BorderPane();
+        BorderPane setcontent = new BorderPane();
 
-        Label label = new Label("Set Content");
-        set.setCenter(label);
+        TableView<Void> table = new TableView<>();
+        TableColumn<Void, Void> well = new TableColumn<>("Well");
+        TableColumn<Void, Void> set = new TableColumn<>("Set");
+        TableColumn<Void, Void> reference = new TableColumn<>("Reference");
+        TableColumn<Void, Void> units = new TableColumn<>("Units");
+        TableColumn<Void, Void> select = new TableColumn<>("Select");
 
-        return set;
+        table.getColumns().addAll(well,set,reference,units,select);
+
+        well.prefWidthProperty().bind(table.widthProperty().multiply(0.2));
+        set.prefWidthProperty().bind(table.widthProperty().multiply(0.2));
+        reference.prefWidthProperty().bind(table.widthProperty().multiply(0.3));
+        units.prefWidthProperty().bind(table.widthProperty().multiply(0.2));
+        select.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
+
+        well.setResizable(false);
+        set.setResizable(false);
+        reference.setResizable(false);
+        units.setResizable(false);
+        select.setResizable(false);
+
+        setcontent.setCenter(table);
+        return setcontent;
     }
 }
