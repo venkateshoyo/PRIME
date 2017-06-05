d7414f3efc5bb4563f7a53f0c3aaf789acc841cb
madHEYsia <submiitr07@gmail.com>
2017-06-06 00:45:14 +0530

    Added dummy constants file

---
+++ src/com/PRIME/main/operations/toolbars/fileMenu/constants.java
@@ -0,0 +1,17 @@
+package com.PRIME.main.operations.toolbars.fileMenu;
+
+public class constants {
+
+    public static final String openFileButtonText = "";
+    public static final String openFileImageURL = "jdbc:mysql://localhost/prime?useSSL=false";
+    public static final String openFileTooltipText = "Open File Button";
+
+    public static final String printButtonText = "";
+    public static final String printImageURL = "jdbc:mysql://localhost/prime?useSSL=false";
+    public static final String printTooltipText = "Print Button";
+
+    public static final String snapshotButtonText = "";
+    public static final String snapshotImageURL = "jdbc:mysql://localhost/prime?useSSL=false";
+    public static final String snapshotTooltipText = "Snapshot Button";
+
+}
