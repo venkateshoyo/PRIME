b9f93d2d6d08f23945a0a0023fdc871e89cc9c4d
harsh <hkbansal1997@gmail.com>
2017-06-21 17:20:16 +0530

    added bind font property

--- src/com/PRIME/main/operations/main/pdfreader/ParsePdfFiles.java
+++ src/com/PRIME/main/operations/main/pdfreader/ParsePdfFiles.java
@@ -2,14 +2,20 @@ package com.PRIME.main.operations.main.pdfreader;
 
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -28,15 +34,14 @@ public class ParsePdfFiles {
     public static void method() {
 
          String folderpath = "\\src\\com\\PRIME\\main\\operations\\main\\pdfreader\\FolderPdf\\";
-        VBox vb = new VBox(20);
+        VBox sp = new VBox(20);
 
         folderpath = System.getProperty("user.dir") + folderpath;
         // System.out.println(System.getProperty("user.dir"));
         PdfReader reader;
         File folder = new File(folderpath);
         File[] listOfFiles = folder.listFiles();
-        SplitPane pane =new SplitPane();
-        pane.setOrientation(Orientation.VERTICAL);
+        String but = "";
         int count =0;
         try {
             for (File file : listOfFiles) {
@@ -46,6 +51,7 @@ public class ParsePdfFiles {
 
                     reader = new PdfReader(filepath);
                     int pages = reader.getNumberOfPages();
+
                     for (int i = 1;i<=pages;i++){
                         String textFromPage = PdfTextExtractor.getTextFromPage(reader, i);
                         Pattern pat = Pattern.compile("([Pp]orosity|por |Por |PHI|phi)(.|\n){1,50}0\\.");
@@ -57,16 +63,10 @@ public class ParsePdfFiles {
                             try {
                                 System.out.println("File: " + filename+", On Page no: "+i);
                                 System.out.println(textFromPage.substring(strt, strt + 300)+"\n\n");
-
                                 String matched = textFromPage.substring(strt, strt + 300);
-
                                 String location= "Found in "+filename+" at page no. "+i;
+                                but = but+location+"\n\n"+matched+"\n\n\n\n\n\n\n";
 
-                                Text tx = new Text(location+"\n\n"+matched);
-                                ScrollPane sp = new ScrollPane(tx);
-                                pane.getItems().add(sp);
-
-                                pane.setDividerPosition(count,0.25*(count+1));
                                 count++;
 
                                 //matches.add(mat.start());
@@ -77,8 +77,18 @@ public class ParsePdfFiles {
                     reader.close();
                 }
             }
-            pane.setPadding(new Insets(10));
+             DoubleProperty fontSize = new SimpleDoubleProperty();
+          IntegerProperty blues = new SimpleIntegerProperty();
+            Label g= new Label(but);
+
+            g.setPadding(new Insets(0,0,0,10));
+            ScrollPane pane = new ScrollPane(new Group(g));
             Scene screen = new Scene(pane);
+            screen.widthProperty().addListener(e->{pane.setPrefWidth(screen.getWidth());});
+            screen.heightProperty().addListener(e->{pane.setPrefHeight(screen.getHeight());});
+            fontSize.bind((screen.widthProperty().divide(100)).add(4));
+            g.styleProperty().bind(Bindings.concat("-fx-font-size: ", fontSize.asString(), ";"
+                    ,"-fx-base: rgb(100,100,",blues.asString(),");"));
             Stage stage = new Stage();
             stage.show();
             stage.setScene(screen);
