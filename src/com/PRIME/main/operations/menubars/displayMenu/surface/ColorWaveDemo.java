6da49c47dc474cec294c06890e93847c9ce2f49b
madHEYsia <submiitr07@gmail.com>
2018-01-22 14:48:37 +0530

    hotfix - WIP

--- src/com/PRIME/main/operations/menubars/displayMenu/surface/ColorWaveDemo.java
+++ src/com/PRIME/main/operations/menubars/displayMenu/surface/ColorWaveDemo.java
@@ -30,8 +30,8 @@ public class ColorWaveDemo {
 
     public BorderPane method() throws IOException {
 
-        Chart chart1 = new Chart(Quality.Advanced, "swing");
-        Chart chart = new Chart(Quality.Advanced, "swing"); // Use "swing" canvas
+        Chart chart1 = new Chart(Quality.Fastest, "swing");
+        Chart chart = new Chart(Quality.Fastest, "swing"); // Use "swing" canvas
 //        Chart chart1 = new Chart(Quality.Fastest, "swing");
 //        Chart chart = new Chart(Quality.Fastest, "swing"); // Use "swing" canvas
 
