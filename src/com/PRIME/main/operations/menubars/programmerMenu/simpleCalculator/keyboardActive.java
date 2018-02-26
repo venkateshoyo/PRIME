d58179a542cbf17539510960c22880bfdb666e83
madHEYsia <submiitr07@gmail.com>
2018-02-26 22:24:10 +0530

    junk - calci keyboard keys activity

---
+++ src/com/PRIME/main/operations/menubars/programmerMenu/simpleCalculator/keyboardActive.java
@@ -0,0 +1,38 @@
+package com.PRIME.main.operations.menubars.programmerMenu.simpleCalculator;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+
+public class keyboardActive {
+
+    public static void keyboardActive(){
+
+//        main.getscene().getAccelerators().put(
+//                new KeyCodeCombination(KeyCode.N, KeyCombination.SHORTCUT_DOWN),
+//                () -> {
+//                    System.out.println("Key pressed: New file");
+//                }
+//        );
+//        main.getscene().getAccelerators().put(
+//                new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN),
+//                () -> {
+//                    System.out.println("Key pressed: Open file");
+//                }
+//        );
+//        main.getscene().getAccelerators().put(
+//                new KeyCodeCombination(KeyCode.W, KeyCombination.SHORTCUT_DOWN),
+//                () -> {
+//                    System.out.println("Key pressed: Exit");
+//                }
+//        );
+//        main.getscene().getAccelerators().put(
+//                new KeyCodeCombination(KeyCode.S, KeyCombination.SHORTCUT_DOWN),
+//                () -> {
+//                    System.out.println("Key pressed: Save");
+//                }
+//        );
+
+    }
+
+}
